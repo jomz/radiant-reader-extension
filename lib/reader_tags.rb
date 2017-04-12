eec9672cc90dba683a3d38511b1e557bab30cd9d
Benny Degezelle <hi@monkeypatch.be>
2017-04-13 00:50:43 +0200

    Add tags for r:reader:postal_address and seperate address fields

--- lib/reader_tags.rb
+++ lib/reader_tags.rb
@@ -78,7 +78,7 @@ module ReaderTags
     tag.expand if tag.locals.reader ||= get_reader(tag)
   end
 
-  [:name, :forename, :surname, :nickname, :preferred_name, :preferred_informal_name, :email, :description, :login, :honorific].each do |field|
+  [:name, :forename, :surname, :nickname, :preferred_name, :preferred_informal_name, :email, :description, :login, :honorific, :post_city, :post_country, :post_province, :postcode, :post_line1, :post_line2, :phone, :mobile ].each do |field|
     desc %{
       Displays the #{field} field of the current reader.
       <pre><code><r:reader:#{field} /></code></pre>
@@ -110,6 +110,23 @@ module ReaderTags
   }
   tag "reader:unless_organisation" do |tag|
     tag.expand if tag.locals.reader.post_organisation.blank?
+  end
+  
+  desc %{
+    Displays the full postal address recorded for the current reader.
+    The 'newline' attribute defaults to a newline (\\n).
+    <pre><code><r:reader:postal_address [newline="<br/>"] /></code></pre>
+  }
+  tag "reader:postal_address" do |tag|
+    if tag.locals.reader
+      newline = tag.attr['newline'] || "\n"
+      tag.render('reader:post_line1') + newline +
+      ((tag.render('reader:post_line2') + newline) unless tag.render('reader:post_line2').blank?).to_s +
+      tag.render('reader:post_city') + newline +
+      tag.render('reader:postcode') + newline +
+      ((tag.render('reader:post_province') + newline) unless tag.render('reader:post_province').blank?).to_s +
+      tag.render('reader:post_country')
+    end
   end
   
   desc %{

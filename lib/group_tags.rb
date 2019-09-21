585df68872aad3f29b70453c29ec2e6a57165bfa
jomz <hi@monkeypatch.be>
2019-09-21 19:06:13 +0200

    Make r:group use tag.locals.group if it exists

--- lib/group_tags.rb
+++ lib/group_tags.rb
@@ -1,38 +1,37 @@
 module GroupTags
   include Radiant::Taggable
   include ReaderHelper
-  
+
   class TagError < StandardError; end
   desc %{
-    The root 'group' tag is not meant to be called directly. 
+    The root 'group' tag is not meant to be called directly.
     All it does is summon a group object so that its fields can be displayed with eg.
-    
+
     <pre><code><r:group:name /></code></pre>
-  
+
     To show a particular group, supply an id or name attribute. There is no access control
     involved here: if you choose to show a group on an unrestricted page, it will appear.
-    
+
     Note that because of limitations in radius this has to be done in a containing root tag:
-    
+
     <pre><code><r:group id="1"><r:group:url /></r:group></code></pre>
   }
   tag 'group' do |tag|
-    tag.locals.group = if tag.attr['id']
-      Group.find_by_id(tag.attr['id'])
+    if tag.attr['id']
+      tag.locals.group = Group.find_by_id(tag.attr['id'])
     elsif tag.attr['name']
-      Group.find_by_name(tag.attr['name'])
+      tag.locals.group = Group.find_by_name(tag.attr['name'])
     elsif @mailer_vars
-      @mailer_vars[:@group]
-    else
-      tag.locals.page.homegroup
+      tag.locals.group = @mailer_vars[:@group]
     end
+    tag.locals.group ||= tag.locals.page.homegroup
     tag.expand if tag.locals.group
   end
 
   [:name, :description, :url].each do |field|
     desc %{
       Displays the #{field} field of the currently relevant group. Works in email messages too.
-    
+
       <pre><code><r:group:#{field} /></code></pre>
     }
     tag "group:#{field}" do |tag|
@@ -42,7 +41,7 @@ module GroupTags
 
   desc %{
     Sets the page scope to this group's home page
-  
+
     <pre><code><r:group:homepage>...</r:group:homepage /></code></pre>
   }
   tag "group:homepage" do |tag|
@@ -51,7 +50,7 @@ module GroupTags
 
   desc %{
     Expands if this group has messages.
-  
+
     <pre><code><r:group:if_messages>...</r:group:if_messages /></code></pre>
   }
   tag "group:if_messages" do |tag|
@@ -60,7 +59,7 @@ module GroupTags
 
   desc %{
     Expands if this group does not have messages.
-  
+
     <pre><code><r:group:unless_messages>...</r:group:unless_messages /></code></pre>
   }
   tag "group:unless_messages" do |tag|
@@ -68,9 +67,9 @@ module GroupTags
   end
 
   desc %{
-    Loops through the non-functional messages (ie not welcomes and reminders) that belong to this group 
+    Loops through the non-functional messages (ie not welcomes and reminders) that belong to this group
     and that have been sent, though not necessarily to the present reader (which is the point, really).
-  
+
     <pre><code><r:group:messages:each>...</r:group:messages:each /></code></pre>
   }
   tag "group:messages" do |tag|
@@ -85,10 +84,10 @@ module GroupTags
     end
     result
   end
-  
+
   desc %{
     Loops through the subgroups of the current group
-  
+
     <pre><code><r:group:subgroups:each>...</r:group:subgroups:each /></code></pre>
   }
   tag "group:subgroups" do |tag|
@@ -104,11 +103,11 @@ module GroupTags
     end
     result.join('')
   end
-  
+
   def group_find_options tag
     attr = tag.attr.symbolize_keys
     options = {}
-    
+
     by = (attr[:by] || 'position').strip
     order = (attr[:order] || 'asc').strip
     order_string = ''
